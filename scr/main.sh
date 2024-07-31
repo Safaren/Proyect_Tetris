b98d328288e2bb37947f6fe7361d336830e7411f
Safaren <sergiomasny@gmail.com>
2024-07-31 11:45:13 +0200

    1 0.01v

--- scr/main.sh
+++ scr/main.sh
@@ -2,3 +2,4 @@
 
 source piece.sh
 source var.sh
+
--- scr/var.sh
+++ scr/var.sh
@@ -1,1 +1,26 @@
 #!/bin/bash
+
+
+# Tamaño de la interface 
+
+WIDTH=20
+HEIGHT=10
+
+
+# COLOR
+
+RED="\033[31m"
+GREEN="\033[32m"
+YELLOW="\033[33m"
+BLUE="\033[34m"
+MAGENTA="\033[35m"
+
+# CODIGOS ESCAPE
+
+# PIECES
+
+# VELOCITY
+
+
+
+
